5110aad4200befafcdfa32c96b54b87f4b2b6c27
James Waples <jamwaffles@gmail.com>
2017-09-30 11:49:54 +0100

    Parse a node on its own

---
+++ doc/format-notes.md
@@ -0,0 +1,4 @@
+# GML
+
+- Comments begin with a `#`
+- Defines a tree structure. Starts with keywork `graph`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,60 @@
 #[macro_use]
 extern crate nom;
 
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::str;
+use std::str::FromStr;
+
+use nom::{ IResult };
+
+named!(node_field<&[u8], (String, String)>, 
+		do_parse!(
+			key: alt!(tag!("id")) >>
+			value: take_until!("\n") >>
+			((String::from_utf8_lossy(key).into_owned(), String::from_utf8_lossy(value).into_owned()))
+		)
+);
+
+named!(node<&[u8], Vec<(String, String)>>, 
+	ws!(
+		do_parse!(
+			tag!("node") >>
+			tag!("[") >>
+			node_fields: many1!(node_field) >>
+			tag!("]") >>
+			(node_fields)
+		)
+	)
+);
+
 fn main() {
-    println!("Hello, world!");
+	let path = Path::new("data/power.gml");
+	let display = path.display();
+
+	let mut file = match File::open(&path) {
+		Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+		Ok(file) => file,
+	};
+
+	let mut s = String::new();
+	file.read_to_string(&mut s).unwrap();
+
+	let test = "node 
+	[
+		id 0
+	]";
+
+	let thing = node(&test.as_bytes());
+
+	match thing {
+		IResult::Done(remaining, parsed) => {
+			println!("{:?}", parsed);
+
+			println!("Remaining bytes: {:?}", remaining.len());
+		},
+		_ => println!("Failed to parse")
+	}
 }
